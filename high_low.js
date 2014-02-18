541e23dc8e8411e84664c35913d219e71652adcf
Laure Fischer <laurefischer27@gmail.com>
2014-02-18 13:19:06 -0500

    initial commit

--- high_low.js
+++ high_low.js
@@ -1,1 +1,16 @@
- 
+var input, solution;
+
+input = prompt('Guess a number between one and ten.');
+solution = '4';
+
+for(;input !== solution;) {
+  if (input > solution) {
+    console.log("Too high!");
+  }
+  else if (input < solution) {
+    console.log("Too low!");
+  }
+  input = prompt("Try again!");
+}
+console.log("Correct!")
+
--- interest_calc.js
+++ interest_calc.js
@@ -1,1 +1,9 @@
- 
+var principal, interest, years, int_payment;
+
+principal = prompt("Amount borrowed?");
+interest = prompt("Interest rate?");
+years = prompt("Number of years?");
+int_payment = principal * interest * years;
+
+console.log(int_payment);
+
